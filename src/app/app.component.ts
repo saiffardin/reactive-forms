8601534df83df3b27019ffb41a43d42eea778925
saiffardin <saiffardin@gmail.com>
2022-03-30 11:54:12 +0600

    nested formGroup || address

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  registrationForm = new FormGroup({
+    userName: new FormControl('Saif'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl(''),
+    }),
+  });
 }
